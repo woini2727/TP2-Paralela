941bc828da42d401060847ac200a57379da6f49d
woini2727 <salvador.woini@gmail.com>
2018-10-25 02:37:33 -0300

    busco el recurso en el cliente

--- tp2/src/clientes/ServidorThreadExtremo.java
+++ tp2/src/clientes/ServidorThreadExtremo.java
@@ -1,5 +1,6 @@
 package clientes;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -9,6 +10,8 @@ import common.Request;
 
 public class ServidorThreadExtremo implements Runnable {
 	Socket sock;
+	File folder;
+	String nFile="dataCliente1.txt";
 	public ServidorThreadExtremo(Socket sock) {
 		this.sock=sock;
 	}
@@ -17,10 +20,27 @@ public class ServidorThreadExtremo implements Runnable {
 		System.out.println("reviso los recursos locales");
 		InputStream is;
 		try {
+			//recibo la request por parte del MASTER
 			is = this.sock.getInputStream();
 			ObjectInputStream ois = new ObjectInputStream(is);
 			Request reqCliente=(Request)ois.readObject();
 			System.out.println(reqCliente.toString());
+			
+			//busco en la lista de archivos del cliente
+			
+			File files= new File("src/clientes/files/"+this.nFile);
+			folder = new File("src/clientes/files/");
+			File[] listOfFiles = folder.listFiles();
+			for (File file : listOfFiles) {
+			    if (file.isFile()) {
+			        if(file.getName().equals((reqCliente).toString())) {
+			        	System.out.println("Encontrado!!");
+			        }else {
+			        	System.out.println("NO ESTA!");
+			        }
+			        	
+			    }
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
